624268f0e5ae1167857fd6fd18610fded530a276
Nathan Smith <ntsmithcs@gmail.com>
2022-12-08 21:03:07 -0500

    part 2

--- Day1/calories.py
+++ Day1/calories.py
@@ -1,11 +1,31 @@
-with open("calories.txt", "r") as f:
-    largest = 0
-    current = 0
-    lines = f.readlines()
-    for line in lines:
-        if line.strip():
-            current += int(line)
-        else:
-            if current > largest:
-                largest = current
-            current = 0
+def main():
+    sums = to_sums("Day1/calories.txt")
+    total = get_total(sums, 3)
+    print(sums)
+    print(total)
+
+
+def to_sums(filename):
+    sums = []
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        current = 0
+        for line in lines:
+            if line.strip():
+                current += int(line)
+            else:
+                sums.append(current)
+                current = 0
+        sums.append(current)
+    return sorted(sums, reverse=True)
+
+
+def get_total(arr, n):
+    total = 0
+    for i in range(n):
+        total += arr[i]
+    return total
+
+
+if __name__ == "__main__":
+    main()
